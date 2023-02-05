be42e30117aa184a17be3e26a3b6d4c12eb83212
Rvjonh <jonhvelasco3@gmail.com>
2023-02-04 22:07:50 -0400

    changed views and urls to viewsets and routers

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,20 @@
-from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 
-class PostList(generics.ListCreateAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
